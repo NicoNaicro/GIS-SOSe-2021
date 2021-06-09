ce7089b54cab36629c537d0224eadec959c8685c
NicoNaicro <Nico-Baindt@gmx.de>
2021-06-09 21:58:50 +0200

    edit

--- Aufgabe3.2/client/script.ts
+++ Aufgabe3.2/client/script.ts
@@ -22,7 +22,7 @@ namespace P_3_2Server {
         _url = _url + "?" + query.toString();
         let answer: Response = await fetch(_url);
         let output: JsonAnswer = await answer.json();
-        displayResponse.innerHTML += "Name: " + output.name + "Mail: " + output.email + " Subject: " +  output.subject;
+        displayResponse.innerHTML += "Name: " + output.name + "Mail: " + output.email + " Betreff: " +  output.subject;
         console.log(answer);
         console.log(output);
         
