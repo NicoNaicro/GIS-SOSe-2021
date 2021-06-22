6c012962aaf2a2470f92f97df5e5725ef1dd4d30
NicoNaicro <Nico-Baindt@gmx.de>
2021-06-22 21:02:49 +0200

    Abgabe34

--- Aufgabe3.2/client/script.ts
+++ Aufgabe3.2/client/script.ts
@@ -1,43 +1,33 @@
 namespace P_3_2Server {
     let displayResponse: HTMLParagraphElement = <HTMLParagraphElement>document.getElementById("answer");
-    async function sendDataHTML(): Promise<void> {
+    async function senddata(): Promise<void> {
         let formData: FormData = new FormData(document.forms[0]);
     
         let _url: RequestInfo = "https://gissigassi.herokuapp.com";
-        _url += "/html";
+        _url += "/sendData";
+        // tslint:disable-next-line: no-any
+        let query: URLSearchParams = new URLSearchParams(<any>formData);
+        _url = _url + "?" + query.toString(); 
+        let answer: Response = await fetch(_url);
+        console.log(answer);  
+    }
+    async function getdata(): Promise<void> {
+        let formData: FormData = new FormData(document.forms[0]);
+     
+        let _url: RequestInfo = "https://gissigassi.herokuapp.com";
+        _url += "/getData";
         // tslint:disable-next-line: no-any
         let query: URLSearchParams = new URLSearchParams(<any>formData);
         _url = _url + "?" + query.toString();
         let answer: Response = await fetch(_url);
         let output: string = await answer.text();
         displayResponse.innerHTML = output;
-    }
-    async function sendDataJSON(): Promise<void> {
-        let formData: FormData = new FormData(document.forms[0]);
-     
-        let _url: RequestInfo = "https://gissigassi.herokuapp.com";
-        _url += "/json";
-        // tslint:disable-next-line: no-any
-        let query: URLSearchParams = new URLSearchParams(<any>formData);
-        _url = _url + "?" + query.toString();
-        let answer: Response = await fetch(_url);
-        let output: JsonAnswer = await answer.json();
-        displayResponse.innerHTML += "Name: " + output.name + "Mail: " + output.email + " Betreff: " +  output.betreff;
-        console.log(answer);
-        console.log(output);
         
 
 
     }
 
-    let sendButtonHTML: HTMLButtonElement = <HTMLButtonElement>document.getElementById("htmlbutton");
-    sendButtonHTML.addEventListener("click", sendDataHTML);
-    let sendButtonJSON: HTMLButtonElement = <HTMLButtonElement>document.getElementById("jsonbutton");
-    sendButtonJSON.addEventListener("click", sendDataJSON);
-
-    interface JsonAnswer {
-        name: string;
-        email: string;
-        betreff: string;
-    }
+    document.getElementById("sendbutton").addEventListener("click", senddata);
+    document.getElementById("getbutton").addEventListener("click", getdata);
+   
 }
--- Aufgabe3.2/server/script.ts
+++ Aufgabe3.2/server/script.ts
@@ -1,42 +1,63 @@
 import * as Http from "http";
 import * as Url from "url";
+import * as Mongo from "mongodb";
 
 export namespace P_3_2Server {
-    console.log("Starting server");
-    let port: number = Number(process.env.PORT);    //neue Variable um sich mit Server zu verbinden
+    let port: number = Number(process.env.PORT);
     if (!port)
-        port = 8100;                                //dem Port 8100 zuweisen
-
-    let server: Http.Server = Http.createServer();  //den Server erstellen
-    server.addListener("request", handleRequest);   //Eventlistener hinzufügen
-    server.addListener("listening", handleListen); 
-    server.listen(port);
-
-    function handleListen(): void {          //Funktion mit Feedback "Listening"       
-        console.log("Listening");
+        port = 8100;
+    startServer(port);
+    function startServer(_port: number | string): void {
+        let server: Http.Server = Http.createServer();
+        console.log("Starting server" + _port);              //den Server erstellen
+        server.addListener("request", handleRequest);       //Eventlistener hinzufügen
+        server.listen(_port);
     }
+    let databaseURL: string = "mongodb+srv://Naicro:<password>@gissigassi.7155q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-
-    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {  //Funktion um Zugriff zu gewähren und gibt URL zurück
+    async function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): Promise <void> {      //Funktion um Zugriff zu gewähren und gibt URL zurück
         _response.setHeader("Access-Control-Allow-Origin", "*");
+        _response.setHeader("content-type", "text/html; charset=utf-8");
         if (_request.url) {
             let url: Url.UrlWithParsedQuery = Url.parse(_request.url, true);
             let path: string = <string>url.pathname;
-            if (path == "/html") {
-                _response.setHeader("content-type", "text/html; charset=utf-8");
-                for (let key in url.query) {
-                    _response.write("<p>" + key + ":" + url.query[key] + "</p>");
-                }
+            let input: Data = {name: url.query.name + " ", mail: url.query.email + " ", subject: url.query.subject + " "};
+            if (path == "/sendData") {
+                let data: string = await sendDatabaseData(databaseURL, input);
+                _response.write(data);
             }
-            else if (path == "/json") {
-                _response.setHeader("content-type", "application/json");
-                let sentObject: string = JSON.stringify(url.query);
-                console.log(sentObject);
-                _response.write(sentObject);
-
+            else if (path == "/getData") {
+               let data: Data[] = await getDatabaseData(databaseURL);                             
+               _response.write(JSON.stringify(data));
+               console.log(data);
+               
             }
         }
         _response.end();
+    }
+    interface Data {
+        name: string;
+        mail: string;
+        subject: string;
+    }
+    async function getDatabaseData(_url: string): Promise<Data[]> {
+        let options: Mongo.MongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
+        let mongoClient: Mongo.MongoClient = new Mongo.MongoClient(_url, options);
+        await mongoClient.connect();
+        let orders: Mongo.Collection = mongoClient.db("LetzteAbgabe").collection("Daten");
+        console.log("Database connection", orders != undefined);
+        let cursor: Mongo.Cursor = orders.find();
+        let data: Data[] = await cursor.toArray();
+        return data;
+    }
+    async function sendDatabaseData(_url: string, _formData: Data): Promise<string> {
+        let options: Mongo.MongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
+        let mongoClient: Mongo.MongoClient = new Mongo.MongoClient(_url, options);
+        await mongoClient.connect();
+        let orders: Mongo.Collection = mongoClient.db("LetzteAbgabe").collection("Daten");
+        orders.insertOne(_formData);
+        let answer: string = "Antwort";
+        return answer;
     }
 
 
