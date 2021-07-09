967f5cf1287fa83840389585a6e6d88b98f872a5
NicoNaicro <Nico-Baindt@gmx.de>
2021-07-09 17:41:18 +0200

    serverfix

--- Memory/deinZeit.js
+++ Memory/deinZeit.js
@@ -1,16 +1,16 @@
 "use strict";
 let button = document.getElementById("submit");
-button.onclick =
-    async function senddata() {
-        let formData = new FormData(document.forms[0]);
-        let _url = "https://gissigassi.herokuapp.com";
-        _url += "/sendData";
-        // tslint:disable-next-line: no-any
-        let query = new URLSearchParams(formData);
-        _url = _url + "?" + query.toString();
-        let answer = await fetch(_url);
-        console.log(answer);
-    };
+button.onclick = senddata;
+function senddata() {
+    let formData = new FormData(document.forms[0]);
+    console.log("click");
+    let _url = "https://gissigassi.herokuapp.com";
+    _url += "/sendData";
+    // tslint:disable-next-line: no-any
+    let query = new URLSearchParams(formData);
+    _url = _url + "?" + query.toString();
+    fetch(_url).then(answer => console.log(answer));
+}
 let time = sessionStorage.getItem("time");
 let timeDOM = document.getElementById("showtime");
 timeDOM.value = time;
--- Memory/deinZeit.ts
+++ Memory/deinZeit.ts
@@ -1,18 +1,18 @@
 
 let button: HTMLElement = document.getElementById("submit");
-
-button.onclick = 
-async function senddata(): Promise<void> {
-    let formData: FormData =  new FormData(document.forms[0]);
+button.onclick = senddata;
+function senddata(): void {
+    let formData: FormData = new FormData(document.forms[0]);
+    console.log("click");
 
     let _url: RequestInfo = "https://gissigassi.herokuapp.com";
     _url += "/sendData";
     // tslint:disable-next-line: no-any
     let query: URLSearchParams = new URLSearchParams(<any>formData);
-    _url = _url + "?" + query.toString(); 
-    let answer: Response = await fetch(_url);
-    console.log(answer);  
-};
+    _url = _url + "?" + query.toString();
+    fetch(_url).then(answer => console.log(answer)
+    );
+}
 let time: string = sessionStorage.getItem("time");
 let timeDOM: HTMLInputElement = document.getElementById("showtime") as HTMLInputElement;
 timeDOM.value = time;
